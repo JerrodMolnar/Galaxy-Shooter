28ef8e02b19c8385571dd5f2a481864b2fedc05e
JerrodMolnar <JerrodMolnar@gmail.com>
2023-02-18 20:05:42 -0500

    added control screen

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -3,8 +3,44 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _titleScreen;
+    [SerializeField] private GameObject _creditsText;
+    [SerializeField] private GameObject _controlsObject;
+
+    private void Start()
+    {
+        if (_titleScreen == null)
+        {
+            Debug.LogError("Title Screen Object not found in MainMenuManager on " + name);
+        }
+        if (_creditsText == null)
+        {
+            Debug.LogError("Credits Text Object not found in MainMenuManager on " + name);
+        }
+        if (_controlsObject == null)
+        {
+            Debug.LogError("Controls Object not found in MainMenuManager on " + name);
+        }
+    }
+
     public void LoadGameScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ShowControls()
+    {
+        if (!_controlsObject.activeSelf)
+        {
+            _creditsText.SetActive(false);
+            _titleScreen.SetActive(false);
+            _controlsObject.SetActive(true);
+        }
+        else
+        {
+            _creditsText.SetActive(true);
+            _titleScreen.SetActive(true);
+            _controlsObject.SetActive(false);
+        }
     }
 }
